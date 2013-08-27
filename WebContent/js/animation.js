023e6f87858e7e924abc9b5e4e58a4f5f4b347f9
hybang <hybang88@hancom.com>
2013-08-27 16:18:39 +0900

    change animation.js

--- WebContent/js/animation.js
+++ WebContent/js/animation.js
@@ -5,7 +5,7 @@ function Animation() {
 Animation.prototype = {
 	init : function() {
 		this.orderArray = new Array();
-		this.event();
+		this.event1();
 	},
 	
 	mappingObjectNAnimation : function(e) {
@@ -14,22 +14,44 @@ Animation.prototype = {
 		var len = this.orderArray.length;
 		this.orderArray[len] = data;
 		console.log(this.orderArray);
+		this.addAnimationList();
+	},
+	
+	addAnimationList : function() {
+		
+	},
+	
+	getSelectedObjectClassName : function(e) {
+		console.log(e);
+		console.log(e.currentTarget.id);
+		this.objectId = e.currentTarget.id;
+		this.event2(this.objectId);
 	},
 	
 	changeAnimationOrder : function() {
-	
+		
 	},
 	
 	playAnimation : function() {
 		
 	}, 
 	
-	event: function() {
-		$(".show").on("click", {oName: "oName", aName: "show"}, $.proxy(this.mappingObjectNAnimation, this));
+	event1: function() {
+/*		$(".show").on("click", {oName: "oName", aName: "show"}, $.proxy(this.mappingObjectNAnimation, this));
 		$(".flyUp").on("click", {oName: "oName", aName: "flyUp"}, $.proxy(this.mappingObjectNAnimation, this));
 		$(".flyDown").on("click", {oName: "oName", aName: "flyDown"}, $.proxy(this.mappingObjectNAnimation, this));
 		$(".flyLeft").on("click", {oName: "oName", aName: "flyLeft"}, $.proxy(this.mappingObjectNAnimation, this));
 		$(".flyRight").on("click", {oName: "oName", aName: "flyRight"}, $.proxy(this.mappingObjectNAnimation, this));
-		$(".fade").on("click", {oName: "oName", aName: "fade"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".fade").on("click", {oName: "oName", aName: "fade"}, $.proxy(this.mappingObjectNAnimation, this));*/
+		$(".doc").delegate("div", "click", $.proxy(this.getSelectedObjectClassName, this));
+	},
+	
+	event2: function(objectId) {
+		$(".show").on("click", {oName: objectId, aName: "show"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".flyUp").on("click", {oName: objectId, aName: "flyUp"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".flyDown").on("click", {oName: objectId, aName: "flyDown"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".flyLeft").on("click", {oName: objectId, aName: "flyLeft"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".flyRight").on("click", {oName: objectId, aName: "flyRight"}, $.proxy(this.mappingObjectNAnimation, this));
+		$(".fade").on("click", {oName: objectId, aName: "fade"}, $.proxy(this.mappingObjectNAnimation, this));
 	}
 };
