84577a5aeee909b99216b62ad414094c7e0c9ad6
hybang <hybang88@hancom.com>
2013-08-28 16:30:47 +0900

    애니메이션 list 순서 변경 추가

--- WebContent/js/animation.js
+++ WebContent/js/animation.js
@@ -3,11 +3,13 @@ function Animation() {
 };
 
 Animation.prototype = {
+	//Animation Class 초기화
 	init : function() {
 		this.orderArray = new Array();
 		this.event();
 	},
 	
+	//Object, Animation 순서에 맞는 배열에 삽입
 	mappingObjectNAnimation : function(e) {
 		var len = this.orderArray.length;
 		
@@ -16,6 +18,7 @@ Animation.prototype = {
 		this.addAnimationList(this.orderArray[len], len);
 	},
 	
+	//Animation list html에 추가
 	addAnimationList : function(newAnimation, order) {
 		$(".aniListWrap").append("<div class='ani1' id='ani" + (order+1) + "'>" + 
 				"<div class='num'>" + (order+1) + "</div>" + 
@@ -24,6 +27,7 @@ Animation.prototype = {
 				"</div>");
 	},
 	
+	//Animation list 삭제
 	deleteAnimationList : function(e) {
 		var aniId = e.currentTarget.parentElement.id;
 		var order = $("#" + aniId).children(".num").text();
@@ -32,22 +36,62 @@ Animation.prototype = {
 		this.deleteMappingObjectNAnimation(order-1);
 	},
 	
+	//orderArray에서 해당 object&Animation 삭제
 	deleteMappingObjectNAnimation : function(order) {
 		var array = this.orderArray.splice(order, 1);
 	},
 	
+	//클릭한 도형의 object ID 얻기
 	getSelectedObjectId : function(e) {
 		this.objectId = e.currentTarget.id;
 	},
 	
+	getSelectedAniListId : function(e) {
+		this.aniListId = e.currentTarget.id;
+	},
+	
+	//Animation list 순서 변경
 	changeAnimationOrder : function(e) {
-
+		console.log(e);
+		console.log(this.orderArray);
+		var order = $("#" + this.aniListId).children(".num").text();
+		var aniText = $("#" + this.aniListId).children(".text").text();
+		
+		console.log(this.aniListId);
+		//aniUp
+		if (e.target.className == "aniUp") {
+			$("#" + this.aniListId).remove();
+			$("<div class='ani1' id='ani" + (order) + "'>" + 
+					"<div class='num'>" + (order - 1) + "</div>" + 
+					"<span class='text'>" + aniText + "</span>" + 
+					"<div class='aniDelete' href='#'></div>" +
+					"</div>").insertBefore("#ani" + (order - 1));
+			$("#ani" + (order - 1)).children(".num").html(order);
+			
+			$("#ani"+(order - 1)).attr("id", "ani" + order);
+			$("#ani"+(order)).attr("id", "ani" + (order - 1));
+		}
+		
+		//aniDown
+		if (e.target.className == "aniDown") {
+//			$("#" + this.aniListId).remove();
+			$("<div class='ani1' id='ani" + order + "'>" + 
+					"<div class='num'>" + (order + 1) + "</div>" + 
+					"<span class='text'>" + aniText + "</span>" + 
+					"<div class='aniDelete' href='#'></div>" +
+					"</div>").insertAfter("#ani" + (order + 1));
+//			$("#ani" + (order)).children(".num").html(order + 1);
+//			
+//			$("#ani"+(order)).attr("id", "ani" + (order + 1));
+//			$("#ani"+(order + 1)).attr("id", "ani" + (order));
+		}
 	},
 	
 	playAnimation : function() {
 		
 	}, 
 	
+	//event
 	event : function() {
 		//도형선택 시 도형 id받아옴
 		$(".doc").on("click", "div", $.proxy(this.getSelectedObjectId, this));
@@ -66,5 +110,8 @@ Animation.prototype = {
 		
 		//애니메이션 list에서 삭제
 		$(".aniListWrap").delegate(".aniDelete", "click", $.proxy(this.deleteAnimationList, this));
+		
+		//애니메이션 list 선택
+		$(".aniListWrap").delegate(".ani1", "click", $.proxy(this.getSelectedAniListId, this));
 	}, 
 };
