e47d8e419e432487902e4ca0104ad5832a41329c
ockjin <okjin-h@hanmail.net>
2013-08-28 17:08:23 +0900

    thumnail slide event 추가

--- WebContent/js/slide/slide.js
+++ WebContent/js/slide/slide.js
@@ -7,17 +7,20 @@ function createSlide(e){
 	$("<div id='thum"+thum_number+"' class='thumbnail1'>&nbsp;</div>").appendTo("#thumbnailbox"+thum_number);
 	
 	$("<div id='doc"+thum_number+"' class='doc'></div>").appendTo(".docWrap");
+	$("#doc"+thum_number).css("display","inline");
 	for(var i=1;i<thum_number;i++){
 		$("#doc"+i).css("display","none");
 	}
-
+	
+	$("#thum1").on("click",$.proxy( this.mainSlideSelect, this, e, 1));
 	$(".thumbWrap").delegate("#thum"+thum_number, "click",$.proxy( this.mainSlideSelect, this, e,thum_number));
+	
+	console.log($(".docWrap"));
 }
 function mainSlideSelect(e, target){
-	console.log(target);
+//	console.log(target);
 	for(var i=1;i<thum_number+1;i++){
 		$("#doc"+i).css("display","none");
 	}
-	$("#doc"+target).css("display","inline");
+	$("#doc"+target).attr("style","display:inline");
 }
-
