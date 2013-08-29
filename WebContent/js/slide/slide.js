413b1f65a92fb7c073673d2317de314ccf20fe72
ockjin <okjin-h@hanmail.net>
2013-08-29 18:07:09 +0900

    slide event update

--- WebContent/js/slide/slide.js
+++ WebContent/js/slide/slide.js
@@ -1,22 +1,73 @@
-var thum_number = 1;
 function createSlide(e){
-	thum_number++;
-	$("<div id='thumbnailbox"+thum_number+"'class='thumbnail'></div>").appendTo(".thumbnails");
-	$("<span id='thumbName"+thum_number+"'class='thumbName'>"+thum_number+"</span>").appendTo("#thumbnailbox"+thum_number);
-	$("<div id='thum"+thum_number+"' class='thumbnail1'>&nbsp;</div>").appendTo("#thumbnailbox"+thum_number);
+	var target = mainSlideSearch();
+	var _target = target+1;
+	var count = $(".docWrap div").length;
+	var _count = count+1;
 	
-	$("<div id='doc"+thum_number+"' class='doc'></div>").appendTo(".docWrap");
-	$("#doc"+thum_number).css("display","inline");
-	for(var i=1;i<thum_number;i++){
-		$("#doc"+i).css("display","none");
+	for(var i=count;i>target;i--){
+		var _num = i+1;
+		$("#thumbnailbox"+i).attr("id","thumbnailbox"+_num);
+		$("#thumbName"+i).text(_num);
+		$("#thumbName"+i).attr("id", "thumbName"+_num);
+		$("#thum"+i).attr("id", "thum"+_num);
+		
+		$("#doc"+i).attr("id","doc"+_num);
+	}
+
+	$("<div id='thumbnailbox"+_target+"'class='thumbnail'></div>").appendTo(".thumbnails");
+	
+	$("<span id='thumbName"+_target+"'class='thumbName'>"+_target+"</span>").appendTo("#thumbnailbox"+_target);
+	$("<div id='thum"+_target+"' class='thumbnail1'>&nbsp;</div>").appendTo("#thumbnailbox"+_target);
+	
+	$("<div id='doc"+_target+"' class='doc'></div>").appendTo(".docWrap");
+	
+	for(var i=_target;i<_count;i++){
+		$("#thumbnailbox"+i).insertBefore("#thumbnailbox"+_count);
+		$("#doc"+i).insertBefore("#doc"+_count);
 	}
 	
+	for(var i=1;i<_count;i++){
+		$("#doc"+i).css("display","none");
+	}
+	$("#doc"+_target).css("display","inline");
+	
+
 	$("#thum1").on("click",$.proxy( this.mainSlideSelect, this, e, 1));
-	$(".thumbWrap").delegate("#thum1", "click",$.proxy( this.mainSlideSelect, this, e,1));
-	$(".thumbWrap").delegate("#thum"+thum_number, "click",$.proxy( this.mainSlideSelect, this, e,thum_number));
+	for(var i=1;i<_count+1;i++){
+		$(".thumbWrap").delegate("#thum"+i,"click", $.proxy(this.mainSlideSelect,this,e,i));
+	}	
 }
+
+function deleteSlide(e){
+	var target = mainSlideSearch();
+	var _target;
+	var count = $(".docWrap div").length;
+	
+	if(target==count){
+		_target = target-1;
+		$("#doc"+target).remove();	
+		$("#doc"+_target).css("display","inline");	
+		$("#thumbnailbox"+target).remove();	
+	}else{
+		_target = target+1;
+		$("#doc"+target).remove();
+		$("#doc"+_target).css("display", "inline");
+		$("#thumbnailbox"+target).remove();
+		
+		for(var i=_target;i<count+1;i++){
+			var _num = i-1;
+			$("#thumbnailbox"+i).attr("id","thumbnailbox"+_num);
+			$("#thumbName"+i).text(_num);
+			$("#thumbName"+i).attr("id", "thumbName"+_num);
+			$("#thum"+i).attr("id", "thum"+_num);
+			$("#doc"+i).attr("id","doc"+_num);
+		}
+	}
+}
+
 function mainSlideSelect(e, target){
-	for(var i=1;i<thum_number+1;i++){
+	var count = $(".docWrap div").length;
+	for(var i=1;i<count+1;i++){
 		$("#doc"+i).css("display","none");
 	}
 	$("#doc"+target).css("display","inline");
@@ -31,34 +82,4 @@ function mainSlideSearch(){
 		}		
 	}
 }
-function deleteSlide(e){
-	var target = mainSlideSearch();
-	var _target;
-	var count = $(".docWrap div").length;
 
-	if(target == count){
-		_target = target-1;
-		$("#doc"+target).remove();	
-		$("#doc"+_target).css("display","inline");	
-		$("#thumbnailbox"+target).remove();	
-		thum_number = thum_number-1;
-	}else{
-		_target = target+1;
-		$("#doc"+target).remove();	
-		$("#doc"+_target).css("display","inline");	
-		$("#thumbnailbox"+target).remove();	
-		
-		thum_number = thum_number-1;
-		for(var i=_target;i<count+1;i++){
-			var _num = i-1;
-			$("#thumbnailbox"+i).attr("id","thumbnailbox"+_num);
-			$("#thumbName"+i).text(_num);
-			$("#thumbName"+i).attr("id", "thumbName"+_num);
-	//		$("#thumbName"+i).html("");
-			$("#thum"+i).attr("id", "thum"+_num);
-			
-			$("#doc"+i).attr("id","doc"+_num);
-		}
-	}
-	
-}
